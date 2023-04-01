b711fc1fb8b1e74d84e96c9804e839f3ceca3131
Camilo Salazar <csalazar@jumpitt.com>
2023-04-01 19:35:39 -0300

    💄 register mockup

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,22 +1,25 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { ImageBackground, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { Button, TextInput, Text, useTheme } from 'react-native-paper';
-
-const backgroundImage = require('../../assets/login-bg.jpg');
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const theme = useTheme();
+  const navigation = useNavigation();
 
   const handleLogin = () => {
   };
 
   const styles = StyleSheet.create({
-    container: {
+    background: {
       flex: 1,
+    },
+    container: {
+      flexGrow: 1,
       padding: 20,
       justifyContent: 'center',
     },
@@ -35,42 +38,57 @@ const LoginScreen = () => {
     button: {
       marginBottom: 20,
     },
+    registerContainer: {
+      flexDirection: 'row',
+      gap: 5,
+    },
     link: {
       color: theme.colors.primary,
+      marginBottom: 20,
     },
   });
 
   return (
-    <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.container}>
-      <View style={{ flex: 2 }}></View>
-      <Text style={styles.title} variant='displayMedium'>Daily Goals</Text>
-      <View style={{ flex: 1 }}></View>
-      <Text style={styles.subtitle} variant='headlineMedium'>Iniciar sesión</Text>
-      <TextInput
-        label="Correo electrónico"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        mode="outlined"
-        style={styles.input}
-      />
-      <TextInput
-        label="Contraseña"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-        mode="outlined"
-        style={styles.input}
-      />
-      <Button mode="contained" onPress={handleLogin} style={styles.button}>
-        Iniciar sesión
-      </Button>
-      <TouchableOpacity
-        onPress={() => { }}
-      >
-        <Text variant='labelLarge' style={styles.link}>¿Olvidaste tu contraseña?</Text>
-      </TouchableOpacity>
-      <View style={{ flex: 2 }}></View>
-    </ImageBackground>
+    <View style={styles.background}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={{ flex: 2 }}></View>
+        <Text style={styles.title} variant='displayMedium'>Daily Goals</Text>
+        <View style={{ flex: 1 }}></View>
+        <Text style={styles.subtitle} variant='headlineMedium'>Iniciar sesión</Text>
+        <TextInput
+          label="Correo electrónico"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          mode="outlined"
+          style={styles.input}
+        />
+        <TextInput
+          label="Contraseña"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          mode="outlined"
+          style={styles.input}
+        />
+        <Button mode="contained" onPress={handleLogin} style={styles.button}>
+          Iniciar sesión
+        </Button>
+        <View style={styles.registerContainer}>
+          <Text>¿No tienes una cuenta?</Text>
+          <TouchableOpacity
+            onPress={() => { navigation.navigate('Registro') }}
+          >
+            <Text variant='labelLarge' style={styles.link}>Crea tu cuenta</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          onPress={() => { }}
+        >
+          <Text variant='labelLarge' style={styles.link}>¿Olvidaste tu contraseña?</Text>
+        </TouchableOpacity>
+        <View style={{ flex: 2 }}></View>
+      </ScrollView>
+    </View >
   );
 
 };
--- src/screens/register.tsx
+++ src/screens/register.tsx
@@ -1,10 +1,130 @@
-import { Text } from "react-native-paper";
-import { View } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Button, TextInput, Text, useTheme } from 'react-native-paper';
 
-export default function RegisterScreen() {
+
+const RegisterScreen = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+  };
+
+  const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+    },
+    container: {
+      flexGrow: 1,
+      padding: 20,
+      justifyContent: 'center',
+    },
+    appBar: {
+      justifyContent: 'flex-start',
+    },
+    backButton: {
+      width: 80,
+    },
+    title: {
+      color: theme.colors.primary,
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+    subtitle: {
+      color: theme.colors.secondary,
+      marginBottom: 20,
+    },
+    input: {
+      marginBottom: 20,
+    },
+    registerButton: {
+      marginBottom: 20,
+    },
+    link: {
+      color: theme.colors.primary,
+    },
+  });
+
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.background}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={{ flex: 2 }}></View>
+        <View style={styles.appBar}>
+          <Button
+            icon="menu-left"
+            onPress={() => { navigation.goBack() }}
+            compact={true}
+            style={styles.backButton}
+          >
+            Atrás
+          </Button>
+        </View>
+        <Text style={styles.title} variant='displayMedium'>Daily Goals</Text>
+        <View style={{ flex: 1 }}></View>
+        <Text style={styles.subtitle} variant='headlineMedium'>Crear cuenta</Text>
+        <TextInput
+          label="Primer nombre"
+          value={firstName}
+          onChangeText={(text) => setFirstName(text)}
+          mode="outlined"
+          style={styles.input}
+        />
+        <TextInput
+          label="Primer apellido"
+          value={lastName}
+          onChangeText={(text) => setLastName(text)}
+          mode="outlined"
+          style={styles.input}
+        />
+        <TextInput
+          label="Correo electrónico"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          mode="outlined"
+          style={styles.input}
+        />
+        <TextInput
+          label="Contraseña"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+          mode="outlined"
+          style={styles.input}
+        />
+        <TextInput
+          label="Confirmar contraseña"
+          value={passwordConfirmation}
+          onChangeText={(text) => setPasswordConfirmation(text)}
+          secureTextEntry
+          mode="outlined"
+          style={styles.input}
+        />
+        <Button
+          mode="contained"
+          onPress={handleLogin}
+          style={styles.registerButton}
+        >
+          Crear cuenta
+        </Button>
+        <TouchableOpacity
+          onPress={() => { }}
+        >
+          <Text variant='labelLarge' style={styles.link}>¿Ya tienes una cuenta?</Text>
+        </TouchableOpacity>
+        <View style={{ flex: 2 }}></View>
+      </ScrollView>
     </View>
   );
-}
+
+};
+
+
+export default RegisterScreen;
